3c3798783bdf92e6303e6c9e1d4935eb9d9cecb3
Romanic Andrea <romanic.andrea@gmail.com>
2019-06-19 02:28:29 +0200

    cloud microservice added

---
+++ spring-cloud-config-microservice/src/main/java/microservices/springcloudconfigmicroservice/SpringCloudConfigMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package microservices.springcloudconfigmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigMicroserviceApplication.class, args);
+    }
+
+}
